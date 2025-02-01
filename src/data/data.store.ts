f75d47ca2ae9f1d39b490fcc0c9420ceadacd1cf
Ventura Mendo <ventura.romendo@gmail.com>
2025-02-01 22:29:22 +0000

    add JSON files to simulate database and TS file to load them

---
+++ src/data/data.store.ts
@@ -0,0 +1,10 @@
+import courses from './courses.json'
+import students from './students.json'
+import ships from './ships.json'
+
+
+// Ships file: Missing Reaper, Pathfinder and Crawler data...
+
+export const database = {
+    courses, students, ships
+}
